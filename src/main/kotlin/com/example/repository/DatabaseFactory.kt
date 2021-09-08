4e2eb68bf7f9ad4064015e4a1c36924e18a30499
oucif <oucifmohamed8@gmail.com>
2021-09-08 15:03:24 +0100

    - Fixing a bug to connect with postgresql

--- src/main/kotlin/com/example/repository/DatabaseFactory.kt
+++ src/main/kotlin/com/example/repository/DatabaseFactory.kt
@@ -4,12 +4,20 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
 
 object DatabaseFactory {
 
     fun init() {
+
+        val uri = URI(System.getenv("DATABASE_URL"))
+        val username = uri.userInfo.split(":").toTypedArray()[0]
+        val password = uri.userInfo.split(":").toTypedArray()[1]
+
+        val jdbcUrl =
+            "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
         Database.connect(
-            url = System.getenv("DATABASE_URL"),
+            url = jdbcUrl,
             driver = System.getenv("JDBC_DRIVER")
         )
     }
